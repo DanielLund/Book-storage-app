96d155566a5c1ec96b73ac9a1f2dc40ade7be89a
Daniel Lund <75539890+DanielLund@users.noreply.github.com>
2021-01-01 14:26:03 +0000

    Add files via upload

--- database.py
+++ database.py
@@ -1,59 +1,46 @@
 """
 this is for storing and retrieving books from a list
 """
-import sqlite3
+from .Database_Connection import DatabaseConnection
 
 
 def create_book_table():
-    connection = sqlite3.connect('data.db')
-    cursor = connection.cursor()
+    with DatabaseConnection('data.db') as connection:
+        cursor = connection.cursor()
 
-    cursor.execute('CREATE TABLE IF NOT EXISTS books(title text, author text, read integer)')
+        cursor.execute('CREATE TABLE IF NOT EXISTS books(title text, author text, read integer)')
 
-    connection.commit()
-    connection.close()
 
 
 def get_all_books():
-    connection = sqlite3.connect('data.db')
-    cursor = connection.cursor()
+    with DatabaseConnection('data.db') as connection:
+        cursor = connection.cursor()
 
-    cursor.execute('SELECT * FROM books')
-    books = [{'title': row[0], 'author': row[1], 'read': row[2]} for row in cursor.fetchall()]
+        cursor.execute('SELECT * FROM books')
+        books = [{'title': row[0], 'author': row[1], 'read': row[2]} for row in cursor.fetchall()]
 
-    connection.close()
 
     return books
 
 
 def add_book(title, author):
-    connection = sqlite3.connect('data.db')
-    cursor = connection.cursor()
+   with DatabaseConnection('data.db') as connection:
+        cursor = connection.cursor()
 
-    cursor.execute('INSERT INTO books VALUES(?, ?, 0)', (title, author))
+        cursor.execute('INSERT INTO books VALUES(?, ?, 0)', (title, author))
 
     #using format string is bad because it allows for SQL injection attacks using DROP TABLE command and others.
 
-    connection.commit()
-    connection.close()
-
 
 def mark_book_read(title):
-    connection = sqlite3.connect('data.db')
-    cursor = connection.cursor()
+    with DatabaseConnection('data.db') as connection:
+        cursor = connection.cursor()
 
-    cursor.execute('UPDATE books SET read=1 WHERE title=?', (title,))
-
-    connection.commit()
-    connection.close()
+        cursor.execute('UPDATE books SET read=1 WHERE title=?', (title,))
 
 
 def delete_book(title):
-    connection = sqlite3.connect('data.db')
-    cursor = connection.cursor()
+    with DatabaseConnection('data.db') as connection:
+        cursor = connection.cursor()
 
-    cursor.execute('DELETE FROM books WHERE title=?', (title,))
-
-    connection.commit()
-    connection.close()
-
+        cursor.execute('DELETE FROM books WHERE title=?', (title,))
